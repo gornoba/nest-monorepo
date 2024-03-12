4a3c40ad5f0f7e4adbd81d0748b900ea3c6b979c
HyunHoo Im <litinglabs03@gmail.com>
2024-03-12 15:43:05 +0900

    order backend

--- apps/orders/src/orders.service.ts
+++ apps/orders/src/orders.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { CreateOrderRequest } from './dto/create-order.request';
+import { OrdersRepository } from './orders.repository';
 
 @Injectable()
 export class OrdersService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly ordersRepository: OrdersRepository) {}
+
+  async createOrder(requset: CreateOrderRequest) {
+    return await this.ordersRepository.create(requset);
+  }
+
+  async getOrders() {
+    return this.ordersRepository.find({});
   }
 }
